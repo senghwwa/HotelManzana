61f5c54bbfdf0866cd3354da60d495f1c084d566
Seng Hwwa <senghwwa@zana-girl.local>
2019-01-07 17:44:28 +0800

    Initial Commit

---
+++ HotelManzana/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotelManzana
+//
+//  Created by Seng Hwwa on 07/01/2019.
+//  Copyright © 2019 senghwabeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
